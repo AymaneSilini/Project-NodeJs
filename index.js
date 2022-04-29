b3cbaa6f71eb6a8f42093881baf6d4ab4018308f
Aymane Silini <silinia@iut1i-702-a04.u-ga.fr>
2022-04-29 09:22:20 +0200

    wip index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
+const mongoose = require('mongoose');
 const app = express();
+
+const categoryRouter = require('./routes/category.route');
+const commentRouter = require('./routes/comment.route');
+const discountRouter = require('./routes/discount.route');
+const gameRouter = require('./routes/game.route');
+const orderlineRouter = require('./routes/order-line.route');
+const orderRouter = require('./routes/order.route');
+const platformRouter = require('./routes/platform.route');
+const requirementsRouter = require('./routes/requirements.route');
+const userRouter = require('./routes/user.route');
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+mongoose.connect(DB_CONNECTION)
+.then(()=>console.log("connexion ok to database"))
+.then(app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+}))
+.catch((err)=> console.log(err));
+
+
+app.use('/category', categoryRouter);
+app.use('/comment', commentRouter);
+app.use('/discount', discountRouter);
+app.use('/game', gameRouter);
+app.use('/orderline', orderlineRouter);
+app.use('/order', orderRouter);
+app.use('/platform', platformRouter);
+app.use('/requirements', requirementsRouter);
+app.use('/user', userRouter);
